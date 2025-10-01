d596e1e62af4a360cc735874f27db0052adb0757
TaliSeroussi <talsero104@gmail.com>
2025-10-01 23:09:01 +0300

    added firebase connection

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'constants/app_constants.dart';
 import 'screens/queue_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const QueueApp());
 }
 
