f219a8a90740799dae4be2015c4f3afeec60d4f6
טלי סרוסי <talsero104@gmail.com>
2025-10-26 11:08:49 +0200

    Update parameters.h

--- shared/cpp/parameters.h
+++ shared/cpp/parameters.h
@@ -1,5 +1,1 @@
-#pragma once
 
-// Firebase configuration - DO NOT COMMIT THIS FILE TO GIT
-#define FIREBASE_SECRET "bvNiGOwhdBR2KDerLCSUm1Xu8clmBCM83gGsKXo8"
-#define FIREBASE_URL "https://iot-queue-management-default-rtdb.europe-west1.firebasedatabase.app"
